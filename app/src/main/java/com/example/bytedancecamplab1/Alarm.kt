75ff80c74e131a1a5d1536905499e517e0574397
RedPanda <01213YZL@stu.xjtu.edu.cn>
2025-05-28 23:33:30 +0800

    fix:修复无通知权限问题

--- app/src/main/java/com/example/bytedancecamplab1/Alarm.kt
+++ app/src/main/java/com/example/bytedancecamplab1/Alarm.kt
@@ -4,7 +4,6 @@ import android.app.AlarmManager
 import android.app.PendingIntent
 import android.content.Intent
 import android.icu.util.Calendar
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.util.Log
@@ -17,6 +16,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.provider.Settings
 import androidx.annotation.RequiresApi
 import androidx.core.net.toUri
+import android.Manifest.permission.POST_NOTIFICATIONS
+import android.content.pm.PackageManager
+import androidx.activity.result.contract.ActivityResultContracts
 
 class Alarm : AppCompatActivity() {
 
@@ -35,11 +37,20 @@ class Alarm : AppCompatActivity() {
 
         alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
 
-        // 跳转系统设置页面请求精确闹钟权限
+        // 申请精确闹钟权限
         if (!alarmManager.canScheduleExactAlarms()) {
             val intent = Intent(Settings.ACTION_REQUEST_SCHEDULE_EXACT_ALARM)
             intent.data = "package:${packageName}".toUri()
             startActivity(intent)
+        }
+        //申请应用通知权限
+        val notificationPermissionLauncher = registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) {}
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(POST_NOTIFICATIONS) != PackageManager.PERMISSION_GRANTED) {
+                notificationPermissionLauncher.launch(POST_NOTIFICATIONS)
+            }
         }
 
         timePicker = findViewById(R.id.timePicker)

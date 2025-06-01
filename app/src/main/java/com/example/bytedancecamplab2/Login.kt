7a55505c88d76cc95a5eac20822b27c3e612684b
AnonymousLesserPanda <83444653+AnonymousLesserPanda@users.noreply.github.com>
2025-06-01 21:30:08 +0800

    [FEATURE]完成登录与注册界面demo (#4)
    * feature:添加登录和注册界面
    
    * fix:修复数据库查询与插入失败问题
    
    * feature:修改主界面为登陆界面
    
    * chore:构建工具更新
    
    ---------
    
    Co-authored-by: RedPanda <01213YZL@stu.xjtu.edu.cn>

---
+++ app/src/main/java/com/example/bytedancecamplab2/Login.kt
@@ -0,0 +1,68 @@
+package com.example.bytedancecamplab2
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    private var userName = ""
+    private var password = ""
+    private val userInfo = UserDataBaseHelper(this)
+
+    private lateinit var userNameEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var loginButton: Button
+    private lateinit var registerButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        //绑定组件
+        userNameEditText = findViewById<EditText>(R.id.user_name_input)
+        passwordEditText = findViewById<EditText>(R.id.password_input)
+        loginButton = findViewById<Button>(R.id.login_button)
+        registerButton = findViewById<Button>(R.id.register_button)
+
+        loginButton.setOnClickListener { login() }
+        registerButton.setOnClickListener { register() }
+    }
+
+    private fun login() {
+        userName = userNameEditText.text.toString()
+        password = passwordEditText.text.toString()
+        val res = userInfo.findUserByUserName(userName)
+        if (res.isEmpty()) {
+            show("用户不存在")
+            return
+        }
+        val user = res[0]
+        if (password != user.password) {
+            show("用户名或密码错误")
+            return
+        }
+        // TODO:跳转到后续界面
+    }
+
+    private fun register() {
+        val intent = Intent(this, Register::class.java)
+        startActivity(intent)
+    }
+
+    private fun show(message: String) {
+        Log.i("Login", message)
+    }
+
+}
---
+++ app/src/main/java/com/example/bytedancecamplab2/UserDataBaseHelper.kt
@@ -0,0 +1,78 @@
+package com.example.bytedancecamplab2
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+import android.util.Log
+
+class UserDataBaseHelper(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+    companion object {
+        private const val DATABASE_NAME = "User.db"
+        private const val DATABASE_VERSION = 1
+        private const val TABLE_NAME = "user_info"
+        private val columns = arrayOf("id", "userName", "password")
+    }
+
+    data class UserInfo(
+        val id: Long,
+        val userName: String,
+        val password: String
+    )
+
+    override fun onCreate(db: SQLiteDatabase?) {
+        val createTableQuery = """
+            CREATE TABLE $TABLE_NAME(
+                ${columns[0]} INTEGER PRIMARY KEY AUTOINCREMENT,
+                ${columns[1]} TEXT NOT NULL,
+                ${columns[2]} TEXT NOT NULL
+            )
+        """.trimIndent()
+        db?.execSQL(createTableQuery)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun addUser(userName: String, password: String): Long {
+        val db: SQLiteDatabase = writableDatabase
+        val values = ContentValues().apply {
+            put(columns[1], userName)
+            put(columns[2], password)
+        }
+        return db.insert(TABLE_NAME, null, values)
+    }
+
+    fun findUserByUserName(userName: String): List<UserInfo> {
+        val db: SQLiteDatabase = readableDatabase
+        val cursor: Cursor = db.query(
+            TABLE_NAME,
+            columns,
+            "${columns[1]} = ?",
+            arrayOf(userName),
+            null, null, null
+        )
+        val res = mutableListOf<UserInfo>()
+        try {
+            val idIndex = cursor.getColumnIndex(columns[0])
+            val userNameIndex = cursor.getColumnIndex(columns[1])
+            val passwordIndex = cursor.getColumnIndex(columns[2])
+
+            while (cursor.moveToNext()) {
+                val id = cursor.getLong(idIndex)
+                val userName = cursor.getString(userNameIndex)
+                val password = cursor.getString(passwordIndex)
+                res.add(UserInfo(id, userName, password))
+            }
+        } catch (e: Exception) {
+            Log.e("database", "username查询错误", e)
+        } finally {
+            cursor.close()
+        }
+        return res
+    }
+}

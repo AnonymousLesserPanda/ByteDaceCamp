8c74ba1caf06e704c01f5f5949d158a9b1d7d0f3
RedPanda <001213YZL@stu.xjtu.edu.cn>
2025-06-07 20:50:02 +0800

    chore:添加依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,9 +42,14 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(libs.retrofit2.retrofit)  // 核心包
-    implementation(libs.converter.gson)  // JSON 转换
-    implementation(libs.adapter.rxjava3)  // RxJava 支持
+    implementation(libs.retrofit2.retrofit)
+    implementation(libs.converter.gson)
+    implementation(libs.adapter.rxjava3)
+    implementation(libs.androidx.activity)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.lifecycle.viewmodel.ktx)
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
